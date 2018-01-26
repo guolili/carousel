c572f2a0c7e2d926170febc2da0f80d4477caad8
lilydream <lilydream@13717920517.com>
2018-01-26 22:53:57 +0800

    add

---
+++ javascript/rendom.js
@@ -0,0 +1,27 @@
+ajaxFn('http://localhost:8020/img').then((result) => { // 渲染数据
+	const data = JSON.parse(result);
+	const wrap = document.createElement('div');
+	wrap.id = 'wrap';// id
+	let str = '<ul id="ulist">';
+	let ols = '<ol id="olist">'
+	data.forEach((item, index) => {
+		str += `<li><img src=${item}></li>`;
+		ols += `<li>${index+1}</li>`;
+	})
+	str += '</ul>';
+	ols += '</ol><span id="lef"><</span><span id="rig">></span>';
+	wrap.innerHTML = str;
+	wrap.innerHTML += ols;
+	document.body.appendChild(wrap);
+	setTimeout(function () {// 调用轮播
+		new Carousel({
+			wrap: 'wrap',
+			ulist: 'ulist',
+			olist: 'olist',
+			olis: 'li',
+			imgs: 'img',
+			lef: 'lef',
+			rig: 'rig'
+		})
+	},500);	
+})
